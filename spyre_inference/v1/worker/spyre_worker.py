9c8bfd616db3317bd735025ba7cb807a2cef2f87
Thomas Ortner <boh@zurich.ibm.com>
2026-05-07 20:24:28 +0200

    Rework init_device in TorchSpyreWorker to avoid double-creation of ModelRunner (#146)
    Rework init_device function such that it calls the upstream init_device function, but patches out the CPUModelRunner with our custom SpyreModelRunner
    
    Signed-off-by: Thomas Ortner <boh@zurich.ibm.com>

--- spyre_inference/v1/worker/spyre_worker.py
+++ spyre_inference/v1/worker/spyre_worker.py
@@ -19,6 +19,7 @@ import torch
 from vllm.config import VllmConfig
 from vllm.logger import init_logger
 from vllm.v1.worker.cpu_worker import CPUWorker
+import vllm.v1.worker.cpu_worker as cpu_worker_module
 
 from spyre_inference.custom_ops import register_all
 from spyre_inference.v1.worker.spyre_model_runner import TorchSpyreModelRunner
@@ -54,15 +55,20 @@ class TorchSpyreWorker(CPUWorker):
         # layers will be swapped out with the custom implementations for spyre.
         register_all()
 
-    def init_device(self) -> None:
-        # Call the upstream init_device function for the environment setup
-        super().init_device()
-
-        # Construct Spyre model runner with torch.device("spyre")
-        self.model_runner = TorchSpyreModelRunner(
+    def init_device(self) -> None:                                
+        # Patch the CPUModelRunner with the TorchSpyreModelRunner
+        original = cpu_worker_module.CPUModelRunner
+        cpu_worker_module.CPUModelRunner = lambda *a, **kw: TorchSpyreModelRunner(
             self.vllm_config,
             torch.device("spyre"),
         )
+        try:
+            # We will invoke the upstream init_device method with the
+            # CPUModelRunner patched. This will ensure that everything for the CPUWorker is setup,
+            # but the spyre-specific model runner is instantiated instead.
+            super().init_device()
+        finally:
+            cpu_worker_module.CPUModelRunner = original
 
     def compile_or_warm_up_model(self) -> float:
         # FIXME: Work around for https://github.com/torch-spyre/torch-spyre/issues/1420
